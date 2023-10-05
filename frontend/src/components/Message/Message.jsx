b03da59f40b50ae418fc4317cb4930b15ac0e6e0
rhsajib <sajib.mmebuet@gmail.com>
2023-10-06 03:02:55 +0600

    added css to every message and implemented js to resize text box with writing text

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const Message = ({message}) => {
+const Message = ({ message }) => {
     return (
-        <div className='p-2 my-2 mx-3 bg-slate-300 rounded-md'>
-            <h1>{message.user_id}</h1>
-            <p>{message.message}</p>
-            <p><small>{message.created_at}</small></p>           
+        <div className="p-2 my-2 mx-4 whitespace-pre-line bg-slate-300 rounded-md">
+            {/* whitespace-pre-line is for daisplay lines with spaces and new line breaks */}
+            <p className="">{message.message}</p>
+            <div className="mt-3 flex flex-col">
+                <p className="flex justify-end">
+                    <small>{message.user_id}</small>
+                </p>
+                <p className="flex justify-end">
+                    <small>{message.created_at}</small>
+                </p>
+            </div>
         </div>
     );
 };
--- frontend/src/components/SendMessage/SendMessage.jsx
+++ frontend/src/components/SendMessage/SendMessage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const SendMessage = ({ handleSendMesaage }) => {
     const [messageText, setMessageText] = useState("");
+    const [textareaClasses, setTextareaClasses] = useState("max-h-10");
 
     const handleSendClick = () => {
         // Call the parent's handleSendMessage function to send the message
@@ -9,6 +10,7 @@ const SendMessage = ({ handleSendMesaage }) => {
 
         // Clear the input field by resetting messageText to an empty string
         setMessageText("");
+        setTextareaClasses("max-h-10");
     };
 
     const handleFormSubmit = (e) => {
@@ -18,17 +20,30 @@ const SendMessage = ({ handleSendMesaage }) => {
 
     // ref: https://codingbeautydev.com/blog/react-get-input-value-on-button-click/
     // ref: https://bobbyhadz.com/blog/react-get-input-value
-    // this function will dynamically track every change of message in input box
+    // this function will dynamically track every change of message in text box
     const handleMessageTextChange = (e) => {
         // console.log(e);
-        setMessageText(e.target.value);
+
+        const { value } = e.target;
+
+        setMessageText(value.trim());   // Trim leading and trailing whitespace 
+
+        // or
+        // setMessageText(e.target.value);
+
+        if (value.length !==0) {          
+            setTextareaClasses("max-h-24");
+        } else {
+            setTextareaClasses("max-h-10");
+        }
     };
 
     return (
         <form onSubmit={handleFormSubmit}>
-            <div className="my-3 flex flex-row mx-4">
-                <input
-                    className="placeholder:italic placeholder:text-slate-400 bg-white w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+            <div className="my-3 flex flex-row justify-center mx-4">
+                <textarea
+                    // ref={textAreaRef}  // scroll below to see the implementation of ref
+                    className={`${textareaClasses} resize-none w-full border rounded-md py-2 px-4 shadow-sm placeholder:italic placeholder:text-slate-400 bg-white border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1`}
                     type="text"
                     id="message"
                     name="message"
@@ -37,12 +52,14 @@ const SendMessage = ({ handleSendMesaage }) => {
                     placeholder="Write text..."
                     autoComplete="off"
                 />
+
                 <button
-                    type="submit"
+                    type="text"
                     className="bg-slate-400 rounded-lg ml-3 px-4"
+
                     // onClick={handleSendClick}
                 >
-                    <span>Send</span>
+                    Send
                 </button>
             </div>
         </form>
@@ -50,3 +67,50 @@ const SendMessage = ({ handleSendMesaage }) => {
 };
 
 export default SendMessage;
+
+
+/* 
+
+* the default height of the textarea and change it.
+
+import React, { Component, createRef } from 'react';
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.textAreaRef = createRef(); // Create a ref for the textarea element
+    this.state = {
+      defaultHeight: 0, // Store the default height here
+      currentHeight: 0, // Store the current height here
+    };
+  }
+
+  componentDidMount() {
+    // Access the default height of the textarea element after it has been rendered
+    const defaultHeight = this.textAreaRef.current.clientHeight;
+    this.setState({ defaultHeight }); // Update the state with the default height
+  }
+
+  handleTextAreaResize = () => {
+    // Access the current height of the textarea element
+    const currentHeight = this.textAreaRef.current.clientHeight;
+    this.setState({ currentHeight }); // Update the state with the current height
+  };
+
+  render() {
+    return (
+      <div>
+        <textarea
+          ref={this.textAreaRef}
+          onInput={this.handleTextAreaResize}
+          style={{ height: `${this.state.currentHeight}px` }} // Dynamically adjust the height
+        ></textarea>
+        <p>Default Height: {this.state.defaultHeight}px</p>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
+
+*/
