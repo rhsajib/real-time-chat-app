dcdd3c4d7d994672e9e4fafccae83fe8b08c929b
rhsajib <sajib.mmebuet@gmail.com>
2023-11-10 02:48:30 +0600

    changed port for react from 5173 to 8080

--- chatp-root/frontend/vite.config.js
+++ chatp-root/frontend/vite.config.js
@@ -4,7 +4,22 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+
+    // this is for development
     server: {
-        host: true,
+        watch: {
+            usePolling: true,
+        },
+        host: true, // needed for the Docker Container port mapping to work
+        strictPort: true,
+        port: 8080, // you can replace this port with any port
+    },
+
+    // this is for production
+    // we need to define which port the project will run on when we execute npm run preview
+    // host: true will expose the project in public address
+    preview: {
+        port: 8080, // you can replace this port with any port
+        host: true, // needed for the Docker Container port mapping to work
     },
 });
