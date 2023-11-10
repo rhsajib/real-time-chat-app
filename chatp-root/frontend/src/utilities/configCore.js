8bb8ee74664585697d4181467f7b827a5623af25
rhsajib <sajib.mmebuet@gmail.com>
2023-11-10 16:11:22 +0600

    added alternative environment variaable no .env file exists

--- chatp-root/frontend/src/utilities/configCore.js
+++ chatp-root/frontend/src/utilities/configCore.js
@@ -1,4 +1,4 @@
-export const apiBaseUrl = import.meta.env.VITE_REACT_API_BASE_URL;
-export const apiVersion = import.meta.env.VITE_REACT_API_VERSION;
+export const apiBaseUrl = import.meta.env.VITE_REACT_API_BASE_URL || 'http://127.0.0.1:8000';
+export const apiVersion = import.meta.env.VITE_REACT_API_VERSION || '/api/v1';
 
 // export const apiBaseUrl = "http://127.0.0.1:8000/api/v1";
